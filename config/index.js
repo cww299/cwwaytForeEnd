dd7c2e1964c22aa5a03aad303cda982439ebfe20
cww <chenxxvv@foxmail.com>
2019-09-11 14:04:06 +0800

    配置文件index备注

--- config/index.js
+++ config/index.js
@@ -5,59 +5,41 @@
 const path = require('path')
 
 module.exports = {
-  dev: {
-
-    // Paths
-    assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
-    proxyTable: {
-      // api映射
+  dev: {  //开发模式下的环境
+    assetsSubDirectory: 'static',  // 编译输出的二级目录
+    assetsPublicPath: '/',      // 编译发布的根目录，可配置为资源服务器域名或 CDN 域名
+    proxyTable: {      // 代理接口（可跨域）
       '/api': {   //替换代理地址名称 , 即/api/helloYt 接口代理为：http://localhost:8820/heollYt
         target: 'http://localhost:8820/',   //代理地址
         changeOrigin: true,             //开启代理、是否可跨域
         pathRewrite: { "^/api": "" }    //重写接口，去掉/api
       }
     },
-
-    // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
-    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    port: 8080, // 运行测试页面的端口
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
 
-    
-    /**
-     * Source Maps
-     */
-
-    // https://webpack.js.org/configuration/devtool/#development
     devtool: 'cheap-module-eval-source-map',
 
-    // If you have problems debugging vue-files in devtools,
-    // set this to false - it *may* help
-    // https://vue-loader.vuejs.org/en/options.html#cachebusting
     cacheBusting: true,
 
     cssSourceMap: true
   },
 
-  build: {
-    // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+  build: {   // production 环境
+    index: path.resolve(__dirname, '../dist/index.html'), //编译输入的 index.html 文件
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsRoot: path.resolve(__dirname, '../dist'), // 编译输出的静态资源路径
+    assetsSubDirectory: 'static', // 编译输出的二级目录
+    assetsPublicPath: '/',    // 编译发布的根目录，可配置为资源服务器域名或 CDN 域名
 
-    /**
-     * Source Maps
-     */
 
-    productionSourceMap: true,
-    // https://webpack.js.org/configuration/devtool/#production
+    productionSourceMap: true,  // 是否开启 cssSourceMap
+
     devtool: '#source-map',
 
     // Gzip off by default as many popular static hosts such as
