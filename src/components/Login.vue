fafd27e6698d1fcc5b9d1b127f8d42908c866ea7
cww <chenxxvv@foxmail.com>
2019-09-12 16:32:07 +0800

    用户登录功能

---
+++ src/components/Login.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="center">
+    <h2>登录中心</h2>
+    <el-form ref="loginForm" :model="loginForm" label-width="80px" :rules="rules">
+      <el-form-item label="用户名：" prop="username">
+        <el-input v-model="loginForm.username" placeholder="请输入用户名" clearable></el-input>
+      </el-form-item>
+      <el-form-item label="密码：" prop="pwd">
+        <el-input v-model="loginForm.pwd" type="password" placeholder="请输入密码" clearable></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="login('loginForm')">登录</el-button>
+        <el-button @click="register">注册</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+export default{
+  data(){
+    return {
+      loginForm:{
+        username:'',
+        pwd:'',
+      },
+      rules: {
+        username: [
+          {required: true, message: '用户名不能为空！', trigger: 'blur'}
+        ],
+        pwd: [
+          {required: true, message: '密码不能为空', trigger: 'blur'}
+        ],
+      }
+    }
+  },
+  methods:{
+    login:function(formName){
+      var self = this;
+      this.$refs[formName].validate((verify) => {
+        if (verify) {
+          axios.get('/api/login',{ params: self.loginForm  }).then((r)=>{
+            r = r.data;
+            if(r.code === 0){
+              this.$router.replace('helloYt')
+            }else{
+              self.$message.error('账号或密码错误！');
+            }
+          })
+        }
+      });
+    },
+    register:function(){
+
+    }
+  }
+}
+</script>
+
+<style>
+  .center{
+    width: 500px;
+    border: 1px solid lightgray;
+    margin: 200px auto;
+    padding: 60px;
+    background-color: #7b6a484d;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,33 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import UserList from '@/components/UserList'
+import EditUser from '@/components/EditUser'
+import HelloYt from '@/components/HelloYt'
+import Login from '@/components/Login'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path:'/',
+      name:'login',
+      component: Login,
+    },
+    {
+      path:'/helloYt',
+      name:'helloYt',
+      component: HelloYt,
+    },
+    {
+      path: '/user/userList',
+      name: 'userList',
+      component: UserList
+    },
+    {
+      path: '/editUser',
+      name: 'editUser',
+      component: EditUser,
     }
   ]
 })
