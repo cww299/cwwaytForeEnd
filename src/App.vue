c0a2df6cba697e5fa90ee35dc390bba734d5506a
cww <chenxxvv@foxmail.com>
2019-09-28 12:42:34 +0800

    主界面菜单栏隐藏切换。兼容移动端

--- src/App.vue
+++ src/App.vue
@@ -18,5 +18,10 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  height: 100%;
 }
+  body,html{
+    margin:0px;
+    height: 100%;
+  }
 </style>
--- src/components/HelloYt.vue
+++ src/components/HelloYt.vue
@@ -1,40 +1,101 @@
 <template>
-  <el-container style="height: 900px; border: 1px solid #eee">
-    <el-header style="text-align: right; font-size: 12px">
-      <el-dropdown>
-        <i class="el-icon-user" style="margin-right: 15px">{{user.username}}</i>
-        <el-dropdown-menu slot="dropdown">
-          <el-dropdown-item>个人信息</el-dropdown-item>
-          <el-dropdown-item>退出登录</el-dropdown-item>
-        </el-dropdown-menu>
-      </el-dropdown>
-    </el-header>
+  <el-container class="containerBody">
+    <div class="containerAside">
+      <el-menu
+        :collapse="isCollapse"
+        default-active="2"
+        background-color="#545c64"
+        text-color="#fff"
+        active-text-color="#ffd04b">
+        <el-submenu index="1">
+          <template slot="title">
+            <i class="el-icon-location"></i>
+            <span>导航一</span>
+          </template>
+          <el-menu-item-group>
+            <template slot="title">分组一</template>
+            <el-menu-item index="1-1">选项1</el-menu-item>
+            <el-menu-item index="1-2">选项2</el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item-group title="分组2">
+            <el-menu-item index="1-3">选项3</el-menu-item>
+          </el-menu-item-group>
+          <el-submenu index="1-4">
+            <template slot="title">选项4</template>
+            <el-menu-item index="1-4-1">选项1</el-menu-item>
+          </el-submenu>
+        </el-submenu>
+        <el-menu-item index="2">
+          <i class="el-icon-menu"></i>
+          <span slot="title">导航二</span>
+        </el-menu-item>
+        <el-menu-item index="3" disabled>
+          <i class="el-icon-document"></i>
+          <span slot="title">导航三</span>
+        </el-menu-item>
+        <el-menu-item index="4">
+          <i class="el-icon-setting"></i>
+          <span slot="title">导航四</span>
+        </el-menu-item>
+
+        <el-submenu index="1">
+          <template slot="title">
+            <i class="el-icon-location"></i>
+            <span>导航一</span>
+          </template>
+          <el-menu-item-group>
+            <template slot="title">分组一</template>
+            <el-menu-item index="1-1">选项1</el-menu-item>
+            <el-menu-item index="1-2">选项2</el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item-group title="分组2">
+            <el-menu-item index="1-3">选项3</el-menu-item>
+          </el-menu-item-group>
+          <el-submenu index="1-4">
+            <template slot="title">选项4</template>
+            <el-menu-item index="1-4-1">选项1</el-menu-item>
+          </el-submenu>
+        </el-submenu>
+        <el-menu-item index="2">
+          <i class="el-icon-menu"></i>
+          <span slot="title">导航二</span>
+        </el-menu-item>
+        <el-menu-item index="3" disabled>
+          <i class="el-icon-document"></i>
+          <span slot="title">导航三</span>
+        </el-menu-item>
+        <el-menu-item index="4">
+          <i class="el-icon-setting"></i>
+          <span slot="title">导航四</span>
+        </el-menu-item>
+
+      </el-menu>
+    </div>
 
     <el-container>
-      <el-aside style="background-color: rgb(238, 241, 246);width:200px;">
-        <el-menu :default-openeds="['1', '2']">
-          <el-submenu index="1">
-            <template slot="title"><i class="el-icon-message"></i>导航一</template>
-            <el-menu-item-group>
-              <template slot="title">分组一</template>
-              <el-menu-item index="1-1">选项1</el-menu-item>
-              <el-menu-item index="1-2">选项2</el-menu-item>
-            </el-menu-item-group>
-          </el-submenu>
-          <el-submenu index="2">
-            <template slot="title"><i class="el-icon-menu"></i>导航二</template>
-            <el-menu-item-group>
-              <template slot="title">分组一</template>
-              <el-menu-item index="2-1">选项1</el-menu-item>
-              <el-menu-item index="2-2">选项2</el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="分组2">
-              <el-menu-item index="2-3">选项3</el-menu-item>
-            </el-menu-item-group>
-          </el-submenu>
+      <el-header class="containerHeader">
+        <div class="leftDiv">
+        <span @click="collapseTrans">
+          <i class="el-icon-s-fold"></i>
+        </span>
 
-        </el-menu>
-      </el-aside>
+        </div>
+        <div class="rightDiv">
+          <span>
+            <i class="el-icon-setting"></i>
+          </span>
+          <el-dropdown>
+            <span class="el-dropdown-link">
+              <i class="el-icon-user">&nbsp;{{user.username}}</i>
+            </span>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>个人信息</el-dropdown-item>
+              <el-dropdown-item>退出登录</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </div>
+      </el-header>
+
       <el-main>
         {{ dataTime.day }}天{{ dataTime.hour }}时{{ dataTime.min }}分{{ dataTime.second }}秒
         <template>
@@ -45,6 +106,7 @@
           </el-carousel>
         </template>
       </el-main>
+
     </el-container>
 
   </el-container>
@@ -72,6 +134,7 @@ export default {
         min:min,
         second:second,
       },
+      isCollapse: false,
       user:{
         username:'cww'
       }
@@ -97,18 +160,52 @@ export default {
     }, 1000);
   },
   methods:{
-
+    collapseTrans:function(obj){
+      let self = this;
+      self.isCollapse = !self.isCollapse;
+    }
   }
 }
 </script>
 <style>
-  .el-header {
-    background-color: #B3C0D1;
-    color: #333;
-    line-height: 60px;
+  .containerBody{
+    border: none;
+    bottom: 0px;
+    height: 100%;
   }
-  .el-aside {
-    color: #333;
+  .containerHeader{
+    font-size: 12px;
+    background-color: #393D49;
+    line-height: 60px;
+    padding-left: 0;
+  }
+  .containerHeader span{
+    cursor: pointer;
+    margin: 0px 8px;
+    color: white;
+    padding: 20px;
+    box-sizing: border-box;
+  }
+  .containerHeader span:hover{
+    color:#0099ff;
+    border-top:2px solid white;
+  }
+  .containerHeader .rightDiv{
+    float:right;
+  }
+  .containerHeader .leftDiv{
+    float:left;
+  }
+  .containerAside{
+    background-color: #545c64;
+    width: auto;
+    overflow: scroll;
+  }
+  .containerAside .el-menu:not(.el-menu--collapse) {
+    width: 200px;
+  }
+  .el-menu{
+    border-right:none;
   }
   .el-carousel__item h3 {
     color: #475669;
@@ -123,5 +220,11 @@ export default {
 
   .el-carousel__item:nth-child(2n+1) {
     background-color: #d3dce6;
+  }
+  @media screen and (max-width: 480px){
+    .el-menu--collapse {
+      width: 0px;
+      overflow: hidden;
+    }
   }
 </style>
