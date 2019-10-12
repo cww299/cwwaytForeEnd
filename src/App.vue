b23d0c9cdb9c87dae216d8fb617c94481d88b071
cww <chenxxvv@foxmail.com>
2019-10-12 13:48:36 +0800

    Signed-off-by: cww <chenxxvv@foxmail.com>

--- src/App.vue
+++ src/App.vue
@@ -18,14 +18,14 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  height: 100%;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    height: 100%;
+  }
   body,html{
     margin:0px;
     height: 100%;
