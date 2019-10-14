75a979da96e38743e18331bcee0416ccfe848830
cww <chenxxvv@foxmail.com>
2019-10-14 11:12:52 +0800

    用户修改功能

--- src/components/EditUser.vue
+++ src/components/EditUser.vue
@@ -1,17 +1,17 @@
 <template>
   <div>
-    <el-form  v-model="user" label-width="80px">
+    <el-form ref="user" :model="user" label-width="80px" :rules="rules">
       <el-form-item label="ID：">
         <el-input v-model="user.id" :disabled="true"></el-input>
       </el-form-item>
-      <el-form-item label="用户名：">
+      <el-form-item label="用户名：" prop="username">
         <el-input v-model="user.username"></el-input>
       </el-form-item>
-      <el-form-item label="密码：">
+      <el-form-item label="密码：" prop="pwd">
         <el-input v-model="user.pwd"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="update">修改</el-button>
+        <el-button type="primary" @click="update('user')">修改</el-button>
         <el-button @click="back">返回</el-button>
       </el-form-item>
     </el-form>
@@ -25,11 +25,34 @@ import axios from 'axios'
     data () {
       return {
         user: this.$route.params,
+        rules: {
+          username: [
+            {required: true, message: '用户名不能为空！', trigger: 'blur'}
+          ],
+          pwd: [
+            {required: true, message: '密码不能为空', trigger: 'blur'}
+          ],
+        }
       }
     },
     methods:{
-      update: function(){
-        this.$message.error('该功能还未完善');
+      update: function(user){
+        var self = this;
+        this.$refs[user].validate((verify) => {
+          if(verify){
+            axios.get('/api/saveUser',{ params: self.user}).then((r)=>{
+              r = r.data;
+              if(r.code==0){
+                self.$message({
+                  message: r.msg,
+                  type: 'success',
+                })
+              }else{
+                self.$message.error(r.msg);
+              }
+            })
+          }
+        })
       },
       back:function(){
         this.$router.push({
--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="hello">
+  <div>
     <h1>{{ msg }}</h1>
     <el-form :inline="true" :model="userForm" ref="userForm" class="demo-form-inline">
       <el-form-item label="用户名">
@@ -9,38 +9,44 @@
         <el-button type="primary" @click="submitForm('userForm')">查询</el-button>
       </el-form-item>
     </el-form>
-
-    <el-table :data="userData" style="width: 40%;margin:0 auto;" border>
-      <el-table-column prop="id"
-                       label="ID"
-                       align="center"></el-table-column>
-      <el-table-column prop="username"
-                       label="用户名"
-                       align="center" ></el-table-column>
-      <el-table-column prop="pwd"
-                       label="密码"
-                       align="center"></el-table-column>
-      <el-table-column label="操作"
-                       align="center">
-        <template slot-scope="scope">
-          <el-button size="mini" @click="editUser(scope.$index, scope.row)">编辑</el-button>
-          <el-button size="mini" @click='deleUser(scope.$index)' type="danger">删除</el-button>
-        </template>
-      </el-table-column>
-    </el-table>
-    <div class="block">
-      <el-pagination
-        background
-        @size-change="trunSize"
-        @current-change="jumpPage"
-        :page-sizes="[2, 5, 10]"
-        :current-page="page.currPage"
-        :page-size="page.pageSize"
-        :total="page.total"
-        layout="total, sizes, prev, pager, next, jumper">
-      </el-pagination>
-
-    </div>
+    <el-row type="flex" class="row-bg" justify="space-around">
+      <el-col :xs="24" :sm="24" :md="18" :lg="18" :xl="12">
+        <div class="">
+          <el-table :data="userData" border>
+            <el-table-column prop="id"
+                             label="ID"
+                             align="center"></el-table-column>
+            <el-table-column prop="username"
+                             label="用户名"
+                             align="center" ></el-table-column>
+            <el-table-column prop="pwd"
+                             label="密码"
+                             align="center"></el-table-column>
+            <el-table-column label="操作"
+                             align="center">
+              <template slot-scope="scope">
+                <el-button size="mini" @click="editUser(scope.$index, scope.row)">编辑</el-button>
+                <el-button size="mini" @click='deleUser(scope.$index)' type="danger">删除</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+          <div class="block">
+            <el-pagination
+              background
+              @size-change="trunSize"
+              @current-change="jumpPage"
+              :page-sizes="[2, 5, 10]"
+              :current-page="page.currPage"
+              :page-size="page.pageSize"
+              :total="page.total"
+              layout="total, sizes, prev, pager, next, jumper">
+            </el-pagination>
+          </div>
+        </div>
+      </el-col>
+    </el-row>
+    
+    
   </div>
 </template>
 
@@ -61,6 +67,8 @@ export default {
         username:''
       }
     }
+  },
+  mounted: function(){
   },
   methods: {
     submitForm: function(formName){
@@ -91,7 +99,7 @@ export default {
       this.$router.push({
         name:'sysEditUser',
         params: {
-          'id': '1',
+          'id': data.id,
           'username': data.username,
           'pwd': data.pwd,
         },
@@ -109,7 +117,6 @@ export default {
           message: '删除成功!'
         })
       }).catch(()=>{
-
       })
     }
   }
